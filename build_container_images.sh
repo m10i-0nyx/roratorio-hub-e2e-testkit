da327a40e5fd70a81ed0ade8688e46732cd68f1a
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-09-20 08:04:24 +0900

    [add] Add build_container_images.sh AWS ECRにpublish

---
+++ build_container_images.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export AWS_ACCOUNT_ID=125536519930
+export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+export AWS_DEFAULT_REGION=ap-northeast-1
+
+aws ecr get-login-password --region $AWS_DEFAULT_REGION \
+  | podman login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+
+aws ecr create-repository \
+  --repository-name roratorio-hub/e2e-testkit \
+  --region $AWS_DEFAULT_REGION
+
+# Build x64 container image and push to ECR
+podman build --tag roratorio-hub/e2e-testkit:latest-x64 \
+  --platform=linux/amd64 \
+  --force-rm \
+  -f Containerfile .
+
+podman tag roratorio-hub/e2e-testkit:latest-x64 \
+  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-x64
+podman push \
+  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-x64
+
+# Build arm64 container image and push to ECR
+podman build --tag roratorio-hub/e2e-testkit:latest-arm64 \
+  --platform=linux/arm64 \
+  --force-rm \
+  -f Containerfile .
+
+podman tag roratorio-hub/e2e-testkit:latest-arm64 \
+  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-arm64
+podman push \
+  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-arm64
