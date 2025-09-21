f097c34408df2c6464fc4bfd3e7d7ebfb270704d
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-09-21 09:57:05 +0900

    [modify] Update build_container_images.sh

--- build_container_images.sh
+++ build_container_images.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-export AWS_ACCOUNT_ID=125536519930
 export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
-export AWS_DEFAULT_REGION=ap-northeast-1
 
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} \
-  | podman login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+aws ecr-public get-login-password --region us-east-1 \
+  | podman login --username AWS --password-stdin public.ecr.aws/u3l2i4k1
 
-aws ecr create-repository \
-  --repository-name roratorio-hub/e2e-testkit \
-  --region ${AWS_DEFAULT_REGION}
+aws ecr-public create-repository \
+  --repository-name m10i/roratorio-hub-e2e-testkit \
+  --region us-east-1
 
 # Build amd64 container image and push to ECR
 podman build --pull-always --tag roratorio-hub/e2e-testkit:latest-amd64 \
@@ -19,9 +17,9 @@ podman build --pull-always --tag roratorio-hub/e2e-testkit:latest-amd64 \
   -f Containerfile .
 
 podman tag roratorio-hub/e2e-testkit:latest-amd64 \
-  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-amd64
+  public.ecr.aws/u3l2i4k1/m10i/roratorio-hub-e2e-testkit:latest-amd64
 podman push \
-  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-amd64
+  public.ecr.aws/u3l2i4k1/m10i/roratorio-hub-e2e-testkit:latest-amd64
 
 # Build arm64 container image and push to ECR
 podman build --pull-always --tag roratorio-hub/e2e-testkit:latest-arm64 \
@@ -30,6 +28,6 @@ podman build --pull-always --tag roratorio-hub/e2e-testkit:latest-arm64 \
   -f Containerfile .
 
 podman tag roratorio-hub/e2e-testkit:latest-arm64 \
-  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-arm64
+  public.ecr.aws/u3l2i4k1/m10i/roratorio-hub-e2e-testkit:latest-arm64
 podman push \
-  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/roratorio-hub/e2e-testkit:latest-arm64
+  public.ecr.aws/u3l2i4k1/m10i/roratorio-hub-e2e-testkit:latest-arm64
